8242f00f26acb01671dcb338c07456dc2846750c
pijey <pjvilloud-bitbucket@yahoo.fr>
2015-04-09 18:21:13 +0200

    Test gh-pages

--- app/adapters/application.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-var ApplicationAdapter = DS.LSAdapter.extend({
-  namespace: 'api'
-});
-
-export default ApplicationAdapter;
--- app/helpers/date-format.js
+++
@@ -1,5 +0,0 @@
-import Ember from "ember";
-
-export default Ember.Handlebars.makeBoundHelper(function(date, format) {
-  return moment(date).format(format);
-});
